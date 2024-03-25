e8468a2608975f10c1f30c81dc56e0f70e208b95
JP <3285470+JPtenBerge@users.noreply.github.com>
2024-03-25 13:14:34 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    if (document.getElementById("member").checked) {
+        document.getElementById("skills").style.display = "block";
+    } else {
+        document.getElementById("skills").style.display = "none";
+    }
+} {
+
+}
